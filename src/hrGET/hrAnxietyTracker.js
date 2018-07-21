ac2b69ab257ccc0cc553ed4deed3a27992cad976
lebentle <lebentle@uwaterloo.ca>
2018-07-21 11:16:14 -0700

    temp

--- src/hrGET/hrAnxietyTracker.js
+++ src/hrGET/hrAnxietyTracker.js
@@ -1,7 +1,32 @@
 var FitBitApiCLient = require("fitbit-node")
+const app = express(); 
 
 
 // don't push to git 
-new FitBitApiCLient({clientId: process.env.clientId, clientSecret:process.env.clientId, apiVersion: "1.2"}) 
+const client = new FitBitApiCLient({clientId: process.env.clientId, clientSecret:process.env.clientId, apiVersion: "1.2"}) 
 
-getAuthorizeUrl()
+
+
+// redirect the user to the Fitbit authorization page
+app.get("/authorize", (req, res) => {
+	// request access to the user's activity, heartrate, location, nutrion, profile, settings, sleep, social, and weight scopes
+	res.redirect(client.getAuthorizeUrl('activity heartrate location nutrition profile settings sleep social weight', 'http://localhost:3000/callback'));
+});
+
+// handle the callback from the Fitbit authorization flow
+app.get("/callback", (req, res) => {
+	// exchange the authorization code we just received for an access token
+	client.getAccessToken(req.query.code, 'http://localhost:3000/callback').then(result => {
+		// use the access token to fetch the user's profile information
+		client.get("/profile.json", result.access_token).then(results => {
+			res.send(results[0]);
+		}).catch(err => {
+			res.status(err.status).send(err);
+		});
+	}).catch(err => {
+		res.status(err.status).send(err);
+	});
+});
+
+// launch the server
+app.listen(3000);
