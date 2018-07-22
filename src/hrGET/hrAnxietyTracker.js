99f8ebb027f12ac95988d50a27040579a19b3487
lebentle <lebentle@uwaterloo.ca>
2018-07-22 09:27:03 -0700

    temp

--- src/hrGET/hrAnxietyTracker.js
+++ src/hrGET/hrAnxietyTracker.js
@@ -26,7 +26,7 @@ app.get('/', (req, res) => res.send('Hello World!'))
 // redirect the user to the Fitbit authorization page
 app.get("/authorize", (req, res) => {
     // request access to the user's activity, heartrate, location, nutrion, profile, settings, sleep, social, and weight scopes
-    return res.redirect(client.getAuthorizeUrl('activity heartrate location nutrition profile settings sleep social weight', 'http://localhost:3000/callback'))
+    res.redirect(client.getAuthorizeUrl('activity heartrate location nutrition profile settings sleep social weight', 'http://localhost:3000/callback'));
     console.log("This is the result:\n")
     console.log("now doing a callback to get")
 
@@ -38,32 +38,34 @@ app.get("/callback", (req, res) => {
     client.getAccessToken(req.query.code, 'http://localhost:3000/callback').then(result => {
         const access_token = result.access_token
         var accessToken = encodeURIComponent(result.access_token)
-        return res.redirect("/userHomePage?token=" + accessToken)
+        res.redirect("/userHomePage?token=" + accessToken)
     }).catch(err => {
-    	console.log(err)
+    res.status(err.status).send(err);
 });
 });
 
 
 
 app.get("/userHomePage", (req, res) => {
-	console.log(req.query.token)
     var accessToken = req.query.token;
-    var data;    
-    client.get("/activities/heart/date/2018-06-30/1d/1min.json", req.query.token).then(results => {
-    	return res.send(results)
-    	console.log("in here")
-    	client.get("/activities/steps/date/2018-06-30/1d/1min.json",req.query.token).then(results => {
-    	return res.send(results)
-    	}).catch(err => {
-     		console.log("hitting an error")
-     		console.log(err)
-    	 	return res.status(err.status).send(err)
-    	});
+    // going to do two generic queries here
+    /*
+    var heartrate = client.get("/activities/heart/date/2018-06-23/1d/1min.json",req.query.token)
+    var data;
+    heartrate.then(results => {
+    	data[hr] = {results}
     }).catch(err => {
-    	return res.status(err.status).send(err);
+    	res.status(err.status).send(err);
     });
- });
+*/
+    var steps = client.get("/activities/steps/date/today/2018-06-23/1d/1min.json",req.query.token)
+    steps.then(results => {
+    	data[steps] = {results}
+        	}).catch(err => {
+    		res.status(err.status).send(err)
+    	});
+        	res.send(data)
+    })
 
 
 
